67fa3c080ac169c162b9a0b615ca4e2d2836bbe3
AlvaroMorte8888 <alvaromorte8888@gmail.com>
2020-06-02 22:17:14 +0300

    Add contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_172411) do
+ActiveRecord::Schema.define(version: 2020_06_02_185936) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(version: 2020_06_02_172411) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "dates", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
