4806e58008aaa2fe00bb466513a4167446c6fe2b
AlvaroMorte8888 <alvaromorte8888@gmail.com>
2020-06-04 21:02:45 +0300

    Implement article#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do 
+  
   get 'home/index'
   get '/' => 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :about
+  resource :terms, only: [:new, :create]
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
   resources :articles
+
 end
