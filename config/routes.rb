df1f38489b88ed9e4dd62dcb4f2a39f80fb1c67b
AlvaroMorte8888 <alvaromorte8888@gmail.com>
2020-06-05 11:54:53 +0300

    Add terms and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'home/index'
   get '/' => 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
   resources :about
   resource :terms, only: [:new, :create]
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
