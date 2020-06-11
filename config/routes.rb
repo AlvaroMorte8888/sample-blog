016d25edad54a196cea72384f46548df5055803f
AlvaroMorte8888 <alvaromorte8888@gmail.com>
2020-06-11 17:18:36 +0300

    Add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :about
   resource :terms, only: [:new, :create]
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resources :articles
+  resources :articles do 
+    resources :comments
+  end  
 end
